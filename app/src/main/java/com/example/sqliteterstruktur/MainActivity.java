a08a3c0e61a64b08cbdceb498c56cf5e4a68b716
nurcahya2442 <nurcahya2443@gmail.com>
2019-09-11 14:32:26 +0700

    komat kamit

--- app/src/main/java/com/example/sqliteterstruktur/MainActivity.java
+++ app/src/main/java/com/example/sqliteterstruktur/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqliteterstruktur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }finally {
+                    startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+                }
+            }
+        });
+        thread.start();
     }
 }
