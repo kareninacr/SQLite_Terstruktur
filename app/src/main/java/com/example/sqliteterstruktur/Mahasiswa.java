6a3e08986c7ffbd1b752fe80be0b20d851214d55
nurcahya2442 <nurcahya2443@gmail.com>
2019-09-11 15:08:56 +0700

    komat kamit

---
+++ app/src/main/java/com/example/sqliteterstruktur/Mahasiswa.java
@@ -0,0 +1,46 @@
+package com.example.sqliteterstruktur;
+
+public class Mahasiswa {
+    String nomor,nama,tanggal,jenkel,alamat;
+
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getJenkel() {
+        return jenkel;
+    }
+
+    public void setJenkel(String jenkel) {
+        this.jenkel = jenkel;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
--- app/src/main/java/com/example/sqliteterstruktur/MahasiswaList.java
+++ app/src/main/java/com/example/sqliteterstruktur/MahasiswaList.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DetailData extends AppCompatActivity {
+public class MahasiswaList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_data);
+        setContentView(R.layout.activity_data_mahasiswa);
     }
 }
